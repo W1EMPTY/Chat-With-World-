0043a784d75eb0f086ec9b61ec771ec309e1f16e
w1empty <15338201850@163.com>
2023-08-03 14:34:18 +0800

    08.03c1

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    
   },
   chat_box_swap(){
     console.log("666");
